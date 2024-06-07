65a8a3b3dca03f98d0a19c27dad6efb6b30eefd0
Ritesh17rb <ritesh17lifeamazing@gmail.com>
2024-06-07 18:52:43 +0530

    added origin-II

--- server.js
+++ server.js
@@ -1,63 +1,47 @@
 import express from 'express';
 import colors from 'colors';
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import morgan from 'morgan';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js';
-import categoryRoutes from './routes/categoryRoutes.js';
-import productRoutes from './routes/productRoutes.js';
-import cors from 'cors';
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
 
 // Configure env
-dotenv.config();
+dotenv.config()
 
-// Database config
+// database config
 connectDB();
 
 // Rest object
-const app = express();
+const app = express()
 
-// Middlewares
+// middlewares
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json())
+app.use(morgan('dev'))
 
-// Universal CORS configuration
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
-  credentials: true,
-  optionsSuccessStatus: 200,
-}));
-
-// Middleware to log requests and handle preflight OPTIONS requests
-app.use((req, res, next) => {
-  console.log(`Handling ${req.method} request to ${req.url}`);
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  if (req.method === 'OPTIONS') {
-    console.log('Responding to preflight request');
-    return res.sendStatus(200); // Respond OK to preflight requests
-  }
-  next();
-});
-
-app.use(express.json());
-app.use(morgan('dev'));
-
-// Routes
+// routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category', categoryRoutes);
-app.use('/api/v1/product', productRoutes);
+app.use("/api/v1/product", productRoutes);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send({
-    message: 'Welcome to green delight',
-  });
-});
+    message: "Welcome to green delight"
+  })
+})
 
 // PORT
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
   console.log(`Server Running on PORT : ${PORT}`.bgCyan.white);
-});
+})
