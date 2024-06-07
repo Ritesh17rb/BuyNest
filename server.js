5f5ed9e0df701c5f612663d48f01391e6aa7aca8
Ritesh17rb <ritesh17lifeamazing@gmail.com>
2024-06-07 18:40:04 +0530

    added origin

--- server.js
+++ server.js
@@ -1,47 +1,48 @@
 import express from 'express';
 import colors from 'colors';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js';
-import categoryRoutes from "./routes/categoryRoutes.js";
-import productRoutes from "./routes/productRoutes.js";
-import cors from "cors";
+import categoryRoutes from './routes/categoryRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import cors from 'cors';
 
 // Configure env
-dotenv.config()
+dotenv.config();
 
-// database config
+// Database config
 connectDB();
 
 // Rest object
-const app = express()
+const app = express();
 
-// middlewares
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials:true,
-})
-);
-app.use(express.json())
-app.use(morgan('dev'))
+// Middlewares
+const corsOptions = {
+  origin: ['https://buynest-seven.vercel.app', 'https://buynest-2ccye0skj-riteshs-projects-58a4d698.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+  optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+};
 
-// routes
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category', categoryRoutes);
-app.use("/api/v1/product", productRoutes);
+app.use('/api/v1/product', productRoutes);
 
-app.get("/",(req,res)=>{
-   res.send({
-    message:"Welcome to green delight"
-   })
-})
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Welcome to green delight',
+  });
+});
 
 // PORT
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT,()=>{
-      console.log(`Server Running on PORT : ${PORT}`.bgCyan.white);
-})
+app.listen(PORT, () => {
+  console.log(`Server Running on PORT : ${PORT}`.bgCyan.white);
+});
